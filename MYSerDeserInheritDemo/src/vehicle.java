6f7bd33a1cd078207a770562caaac7dff84725bc
RPLVihanga <103477094+RPLVihanga@users.noreply.github.com>
2023-05-27 15:39:36 +0530

    createVehicle_Class

---
+++ MYSerDeserInheritDemo/src/vehicle.java
@@ -0,0 +1,18 @@
+public class vehicle {
+    private String color;
+
+    public vehicle(){
+
+    }
+    public vehicle(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
